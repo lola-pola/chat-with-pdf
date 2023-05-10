a795c74ce2d04c238934c3916c595ce0e01ac8ae
shdowofdeath <elhayefrat@gmail.com>
2023-05-10 15:30:57 +0300

    adding
    Signed-off-by: shdowofdeath <elhayefrat@gmail.com>

---
+++ app/app.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import openai
+import PyPDF2
+import numpy as np
+import os
+
+
+# Set up OpenAI API key
+openai.api_type = "azure"
+openai.api_base = "https://biotest123.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key = os.getenv("KEY_AZURE_AI")
+
+# Define function to extract text from PDF file
+def extract_text(pdf_file):
+    with pdf_file:
+        read_pdf = PyPDF2.PdfReader(pdf_file)
+        text = ""
+        for page in read_pdf.pages:
+            text += page.extract_text()
+        return text
+
+
+# Define function to generate embeddings for text
+def generate_embeddings(text):
+    model_engine = "yuval-dev"  # can also use "davinci" for non-coding tasks
+    prompt = f"summeize the document:\n{text}"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=2000,
+        temperature=0.5
+    )
+    # embeddings = np.array(response.choices[0].embedding).astype(np.float32)
+    embeddings = np.array(response.choices[0])
+    return embeddings
+
+
+# Define Streamlit app
+def app():
+    st.set_page_config(page_title="OpenAI PDF Reader")
+
+    st.title("OpenAI PDF Reader")
+
+    # Upload PDF file
+    pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+    if pdf_file:
+        # Extract text from PDF file
+        text = extract_text(pdf_file)
+
+        # Generate embeddings for text using OpenAI API
+        # embeddings = generate_embeddings(text)
+        embeddings =text
+        
+        # Display text
+        with st.expander("Show text"):
+            st.write(text)
+        
+        # Ask a question using OpenAI API and compare against the generated embeddings
+        question = st.text_input("What would you like to ask?")
+        if st.button("Ask"):
+            response = openai.Completion.create(
+                engine="yuval-dev",
+                # prompt=f"doc summery {embeddings.tolist()} Q: {question}\nA:",
+                prompt=f"Q: {question} \n on this doc summery {embeddings}\nA:",
+
+                max_tokens=2000,
+                stop=None,
+                temperature=1
+                )
+
+            # Display answer
+            print(question)
+            answer = response.choices[0].text.strip()
+            st.write(f"Answer: {answer}")
+
+
+def main():
+    app()
+
+
+if __name__ == '__main__':
+    main()
